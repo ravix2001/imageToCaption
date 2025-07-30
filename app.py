7c8098026392b9fcdaba8a74c27f9d4a6ad1c03a
ravix2001 <ravipandit2001.11.02@gmail.com>
2025-07-30 22:33:31 +0545

    mem

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def generate():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5050))  # Use PORT env var from Render
+    port = int(os.environ.get("PORT", 5000))  # Use PORT env var from Render
     app.run(host="0.0.0.0", port=port)
